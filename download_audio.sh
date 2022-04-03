52be453ddee1c2573ffe56417d79c145baacd493
p3zo <p3zo@users.noreply.github.com>
2022-04-02 17:51:17 -0700

    Add utility scripts for preparing target audio

---
+++ download_audio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yt-dlp \
+    --id "qP-7GNoDJ5c" \
+    --audio-format wav \
+    --extract-audio \
+    -v
---
+++ trim_and_resample.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ffmpeg -t 10 -ss 00:00:02.000 -i qP-7GNoDJ5c.wav -ar 44100 short_qP-7GNoDJ5c.wav
